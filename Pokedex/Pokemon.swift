1dd36c497f72c82845ca2de394338d9069064964
Evgeny Vlasov <564n@Evgenys-MacBook-Pro.local>
2016-09-29 23:30:16 -0700

    2nd VC's layout set up with stack views

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,36 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Evgeny Vlasov on 9/27/16.
+//  Copyright © 2016 Evgeny Vlasov. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    fileprivate var _name: String!
+    fileprivate var _pokedexId: Int!
+    fileprivate var _description: String!
+    fileprivate var _type: String!
+    fileprivate var _defense: String!
+    fileprivate var _height: String!
+    fileprivate var _weight: String!
+    fileprivate var _attack: String!
+    fileprivate var _nextEvolutionTxt: String!
+    
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
---
+++ Pokedex/PokemonDitailVC.swift
@@ -0,0 +1,48 @@
+//
+//  PokemonDitailVC.swift
+//  Pokedex
+//
+//  Created by Evgeny Vlasov on 9/29/16.
+//  Copyright © 2016 Evgeny Vlasov. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDitailVC: UIViewController {
+
+    var pokemon: Pokemon!
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightlbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nameLbl.text = pokemon.name
+        self.view.backgroundColor = UIColor.white
+
+    }
+
+    @IBAction func backBtnPressed(_ sender: AnyObject) {
+    dismiss(animated: true, completion: nil)
+    
+    }
+
+}
+
+
+
+
+
+
